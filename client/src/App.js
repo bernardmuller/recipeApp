e41535331717a984eaca5a3f3e2824ca5d45dde7
Bernard Muller <b.mullerjnr@gmail.com>
2021-07-27 21:26:03 +0200

    'implemented-axios'

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
 import React, {useState, useEffect} from 'react'
+import Axios from 'axios'
 
 function App() {
 
-  const [menus, setMenus] = useState('');
+  const [menus, setMenus] = useState([]);
 
-  const callAPI = () => (
-    fetch("http://localhost:9000/testAPI")
-        .then(res =>  res.text())
-        .then(res => setMenus(res))        
-  )
-
+  
   useEffect(() => {
-    callAPI()
+    fetchMenus()
   }, []);
+
+
+  const fetchMenus = async() => {
+    const response = await Axios("http://localhost:9000/testAPI");
+    console.log(response.data) 
+    setMenus(response.data)   
+  }
 
   return (
     <div className="App"> 
         <h1>client side</h1>     
-        <p>{menus}</p>
+        {
+          menus && menus.map(menu => {
+            return(
+              <p>{menu.name}</p>
+            )
+          })
+        }        
       </div>
   )
 }
